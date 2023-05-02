891429f2175158827c87e2c8be576ac63d36e92c
avashist1998 <abhay.vashist1998@gmail.com>
2023-05-01 23:50:47 -0500

    fix: minor corrections

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -75,32 +75,3 @@
     <NavLi href="/contact">Contact</NavLi>
   </NavUl>
 </Navbar>
-
-<!-- <script>
-  import {
-    Navbar,
-    NavBrand,
-    NavLi,
-    NavUl,
-    NavHamburger,
-    Button,
-    Input,
-  } from "flowbite-svelte";
-  import logo from "/images/logo.png";
-</script>
-
-<Navbar let:hidden let:toggle>
-  <NavBrand href="/">
-    <img src={logo} class="mr-3 h-6 sm:h-9" alt="Logo"/>
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      PodPal
-    </span>
-  </NavBrand>
-  <NavHamburger on:click={toggle} />
-  <NavUl {hidden}>
-    <NavLi href="/" active={true}>Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
-  </NavUl>
-</Navbar> -->
-
--- src/stores/episodeStore.ts
+++ src/stores/episodeStore.ts
@@ -1,6 +1,5 @@
-import {writable, derived} from 'svelte/store';
+import { writable } from 'svelte/store';
 import type EpisodeData from "../lib/types/EpisodeDate";
-
 
 export const episodeStore = writable([] as EpisodeData[]);
 export const selectedEpisodeStore = writable(undefined as EpisodeData | undefined);
